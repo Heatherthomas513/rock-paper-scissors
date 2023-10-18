df6086c9e4bc6a3bf2c561cf5fb82efd533b39c5
heatherthomas513 <heatherthomas513@gmail.com>
2023-10-18 17:01:36 -0400

    Add playerChoice variable and play function
    Add prompt method for player to type in a selection to be stored in playerChoice variable. Started creating the playRound function for comparing the user and computer choicesand returning the results.

--- javascript.js
+++ javascript.js
@@ -12,13 +12,33 @@ let computerChoice;
 
 function getComputerChoice() {
     if (randomNumber === 1) {
-        computerChoice = "Rock";
+        computerChoice = "rock";
     } else if (randomNumber === 2) {
-        computerChoice = "Paper";
+        computerChoice = "paper";
     } else if (randomNumber === 3) {
-        computerChoice = "Scissors";
+        computerChoice = "scissors";
     }
     return computerChoice;
 }
 
 console.log(getComputerChoice());
+
+/* Using prompt() users can type in rock, paper, or scissors.
+The user selection will be stored in the variable playerChoice.
+The function playRound will take playerChoice and computerChoice as its parameters.
+playRound will compare the parameters and return a statement regarding the results*/
+
+let playerChoice = prompt("Rock, paper, or scissors?").toLowerCase();
+console.log(playerChoice);
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === "rock" && computerChoice === "rock") {
+        return "You both chose rock. It's a tie!";
+    } else if (playerChoice === "rock" && computerChoice === "paper") {
+        return "Paper beats rock. You lose.";
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+        return "Rock beats scissors. You win!";
+    }
+}
+
+console.log(playRound(playerChoice, computerChoice));
