7eaaab141066c5144d8bb86685f9b7c98d892bbc
heatherthomas513 <heatherthomas513@gmail.com>
2023-10-19 13:39:30 -0400

    Delete comments and unnecessary line breaks
    Clean code by deleting unecessary comments and line breaks

--- javascript.js
+++ javascript.js
@@ -1,8 +1,3 @@
-/* A game of rock, paper, scissors, playing against the computer
-The function getComputerChoice will return either 'Rock', 'Paper', or 'Scissors'
-getComputerChoice will work by first generating a random number between 1 and 3,
-and then assigning either rock, paper, or scissors depending on the number generated.*/
-
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -21,15 +16,7 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-
-
-/* Using prompt() users can type in rock, paper, or scissors.
-The user selection will be stored in the variable playerChoice.
-The function playRound will take playerChoice and computerChoice as its parameters.
-playRound will compare the parameters and return a statement regarding the results*/
-
 let playerChoice;
-
 getComputerChoice();
 
 function playRound(playerChoice, computerChoice) {
@@ -53,16 +40,6 @@ function playRound(playerChoice, computerChoice) {
         return "Rock beats scissors. You lose.";
     }
 }
-
-
-
-/* The next function will be called game(), and it will use the previous function
-to create a 5 round game that keeps score and reports a winner or loser at the end.
-A for loop will be used to repeat the function 5 times. 
-To keep score there will be a variable for the playerScore and the computerScore.
-To determine which score variable should have a point added, the playRound function 
-results can be searched for the text 'win', 'lose', or 'tie'.
-The score variables will be compared at the end of the function.*/
 
 let playerScore = 0;
 let computerScore = 0;
@@ -94,6 +71,5 @@ function game() {
             console.log("Game over. You lost.");
         }
     }
-
 
 game();
